4a7465ee2148be940920423471fef1351cd7a277
iulianpro <iulian.prodea@gmail.com>
2020-06-05 08:59:06 +0100

    Deployment part 3

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'USE_DEBUG' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['iulianpro-online-shop.herokuapp.com', '127.0.0.1']
 
